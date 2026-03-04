88d97736636f48f1f0d34632fa1ae4ccbc4ad904
Balakrishna Avulapati <ba@bavulapati.com>
2026-03-04 19:48:41 +0530

    updates
    update to ghostty theme
    enforce swfit indentation to 2 tabs
    
    Signed-off-by: Balakrishna Avulapati <ba@bavulapati.com>

--- modules/nvim/starter/lua/plugins/swift.lua
+++ modules/nvim/starter/lua/plugins/swift.lua
@@ -1,3 +1,9 @@
+-- ~/.config/nvim/after/ftplugin/swift.lua
+
+vim.opt_local.tabstop = 2 -- A tab is 4 spaces
+vim.opt_local.shiftwidth = 2 -- Size of an indent is 4 spaces
+vim.opt_local.softtabstop = 2 -- Number of spaces a <Tab> counts for
+vim.opt_local.expandtab = true -- Use spaces instead of tabs
 return {
   {
     "nvim-treesitter/nvim-treesitter",

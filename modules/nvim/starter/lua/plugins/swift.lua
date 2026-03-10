cb15642c33e0cb346821d9ab8daf3f7642bf773b
Balakrishna Avulapati <ba@bavulapati.com>
2026-03-10 14:54:28 +0530

    add local/bin to path
    Signed-off-by: Balakrishna Avulapati <ba@bavulapati.com>

--- modules/nvim/starter/lua/plugins/swift.lua
+++ modules/nvim/starter/lua/plugins/swift.lua
@@ -1,9 +1,3 @@
--- ~/.config/nvim/after/ftplugin/swift.lua
-
-vim.opt_local.tabstop = 2 -- A tab is 4 spaces
-vim.opt_local.shiftwidth = 2 -- Size of an indent is 4 spaces
-vim.opt_local.softtabstop = 2 -- Number of spaces a <Tab> counts for
-vim.opt_local.expandtab = true -- Use spaces instead of tabs
 return {
   {
     "nvim-treesitter/nvim-treesitter",
@@ -11,7 +5,15 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
+    config = function()
+      vim.opt_local.tabstop = 2 -- A tab is 4 spaces
+      vim.opt_local.shiftwidth = 2 -- Size of an indent is 4 spaces
+      vim.opt_local.softtabstop = 2 -- Number of spaces a <Tab> counts for
+      vim.opt_local.expandtab = true -- Use spaces instead of tabs
+    end,
+
     opts = {
+
       servers = {
         sourcekit = {}, -- Requires Xcode/SourceKit-LSP installed
       },

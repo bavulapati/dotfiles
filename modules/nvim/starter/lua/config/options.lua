6b85d3e5b2ddb88e1f7c40ddda6f74b134135d56
Balakrishna Avulapati <ba@bavulapati.com>
2026-05-01 07:12:26 +0530

    let's not use prettier if there is no config
    Signed-off-by: Balakrishna Avulapati <ba@bavulapati.com>

--- modules/nvim/starter/lua/config/options.lua
+++ modules/nvim/starter/lua/config/options.lua
@@ -5,3 +5,7 @@
 vim.opt.scrolloff = 999
 
 vim.opt.guicursor = "n-v-c-i:block"
+
+-- Enable the option to require a Prettier config file
+-- If no prettier config file is found, the formatter will not be used
+vim.g.lazyvim_prettier_needs_config = false
